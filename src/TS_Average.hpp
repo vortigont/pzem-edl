3f774693115fd35185403a62d1dedcebf45980c5
hosanglee-kr <50106977+hosanglee-kr@users.noreply.github.com>
2024-06-28 08:12:16 +0900

    Update TS_Average.hpp

--- src/TS_Average.hpp
+++ src/TS_Average.hpp
@@ -2,7 +2,33 @@
 
 #include "pzem_modbus.hpp"
 
+/*
+switch(meters->getState(id)->model) {
+        case pzmodel_t::pzem004v3 : {
+            pz004::rx_msg_prettyp(m);       // parse incoming message and print some nice info
 
+            // or we can access struct data for the updated object (an example)
+            auto *s = (const pz004::state*)meters->getState(id);
+            Serial.printf("===\nPower alarm: %s\n", s->alarm ? "present" : "absent");
+            Serial.printf("Power factor: %d\n", s->data.pf);
+            Serial.printf("Current value: %f\n", s->data.asFloat(meter_t::cur));
+            break;
+        }
+        case pzmodel_t::pzem003 : {
+            pz003::rx_msg_prettyp(m);       // parse incoming message and print some nice info
+
+            // or we can access struct data for the updated object
+            auto *s = (const pz003::state*)meters->getState(id);
+            Serial.printf("===\nPower high alarm: %s\n", s->alarmh ? "present" : "absent");
+            Serial.printf("Power low alarm: %s\n", s->alarml ? "present" : "absent");
+            Serial.printf("Energy: %d\n", s->data.energy);
+            Serial.printf("Current value: %f\n", s->data.asFloat(meter_t::cur));
+            break;
+        }
+        default:
+            break;
+    }
+*/
 
 template <class T>
 class AveragingFunction {
@@ -15,14 +41,13 @@ class AveragingFunction {
 	virtual size_t getCnt() const = 0;
 };
 
-
-namespace pz004 {   
-class MeanAverage : public AveragingFunction<pz004::metrics> {
+ 
+class MeanAverage : public AveragingFunction<T> {
 	unsigned v{0}, c{0}, p{0}, e{0}, f{0}, pf{0}, _cnt{0};
 
    public:
-	void		   push(const pz004::metrics& m) override;
-	pz004::metrics get() override;
+	void		   push(const T& m) override;
+	T                  get() override;
 	void		   reset() override;
 	size_t		   getCnt() const override {
 		return _cnt;
@@ -30,24 +55,29 @@ class MeanAverage : public AveragingFunction<pz004::metrics> {
 };
 
 
-void MeanAverage::push(const pz004::metrics& m){
+void MeanAverage::push(const T& m){
+    
     v += m.voltage;
     c += m.current;
     p += m.power;
     e = m.energy;
-    f += m.freq;
-    pf += m.pf;
+    if (m->getState(id)->model == pzmodel_t::pzem004v3){
+       f += m.freq;
+       pf += m.pf;
+    )
     ++_cnt;
 }
 
-pz004::metrics MeanAverage::get(){
-    pz004::metrics _m;
+T MeanAverage::get(){
+    T _m;
     _m.voltage = v / _cnt;
     _m.current = c / _cnt;
     _m.power = p / _cnt;
     _m.energy = e;
-    _m.freq = f / _cnt;
-    _m.pf = pf / _cnt;
+    if (_m.getState(id)->model == pzmodel_t::pzem004v3){
+        _m.freq = f / _cnt;
+        _m.pf = pf / _cnt;
+    }
     return _m;
 }
 
@@ -55,9 +85,9 @@ void MeanAverage::reset(){
     v = c = p = e = f = pf = _cnt = 0;
 }
 
-}
 
 
+/*
 namespace pz003 {   
 class MeanAverage : public AveragingFunction<pz003::metrics> {
 	unsigned v{0}, c{0}, p{0}, e{0}, _cnt{0};
@@ -95,6 +125,8 @@ void MeanAverage::reset(){
 }
 
 }
+
+*/
 
 
 
